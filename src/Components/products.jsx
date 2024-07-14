7cbb6cd7de64e48b750bbd63532f015b501435b3
utibe solomon <utibesolomon6@gmail.com>
2024-07-14 18:16:39 +0100

    Fast Changes

--- src/Components/products.jsx
+++ src/Components/products.jsx
@@ -129,8 +129,8 @@ const navigate = useNavigate()
                
                  <div className="flex gap-x-3">
                     <div className="py-2 px-4 text-white cursor-pointer bg-black rounded-[16px]" onClick={()=>{setdata(Products.data.slice(0,10))}}>1</div>
-                    <div className={`py-2 ${data.length <= 10 ? 'hidden' : ''} px-4 text-black cursor-pointer bg-white rounded-[16px]`} onClick={()=> {setdata(Products.data.slice(11,20))}}>2</div>
-                    <div className={`py-2 px-4 ${data.length <= 20 ? 'hidden' : ''}   text-black cursor-pointer bg-white rounded-[16px]`} onClick={()=> {setdata(Products.data.slice(21,30))}}>3</div>
+                    <div className={`py-2 ${Products.data.length <= 10 ? 'hidden' : ''} px-4 text-black cursor-pointer bg-white rounded-[16px]`} onClick={()=> {setdata(Products.data.slice(11,20))}}>2</div>
+                    <div className={`py-2 px-4 ${Products.data.length <= 20 ? 'hidden' : ''}   text-black cursor-pointer bg-white rounded-[16px]`} onClick={()=> {setdata(Products.data.slice(21,30))}}>3</div>
                  </div>
           
 
